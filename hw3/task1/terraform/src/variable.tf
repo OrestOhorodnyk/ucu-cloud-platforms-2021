e9d57ab7b29abeaa2014bdcac0ad5c30a5551928
Orest Ohorodnyk <o.ohorodnyk@gmail.com>
2021-08-11 13:53:37 +0300

    HW3

---
+++ hw3/task1/terraform/src/variable.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  default = "terraform-321413"
+}
+
+variable "region" {
+  default = "us-central1" 
+}
+
+variable "zone"  {
+  default = "us-central1-c"
+}
+
+variable "cloud-function-bucket" {
+  default = "cloud-function-playground-s-11-db5b7aac"
+} 
